222c2f585e7d146220327841d2fbd5c33d47e9f9
Dror Handler <hdror507@gmail.com>
2023-05-25 15:16:31 +0300

    adding travel interface

---
+++ src/app/models/travel.ts
@@ -0,0 +1,7 @@
+export interface Travel {
+    country: string,
+    startDate: Date,
+    endDate: Date,
+    notes: string,
+    flag?: string
+}
--- src/app/services/travel.service.ts
+++ src/app/services/travel.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Travel } from '../models/travel';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class TravelService {
-
   constructor() { }
+
+  private _travelsDB: Travel[] = [
+    { country: "usa", startDate: new Date(), endDate: new Date(), notes: '' }
+  ]
+
+  private _travels$ = new BehaviorSubject<Travel[]>([])
+  travels$ = this._travels$.asObservable()
+
+
 }
